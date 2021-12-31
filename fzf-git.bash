d77ebb21cda35b41a4e08ce1caa9807230a2b8b8
Chad Paradis <cparadis6191@gmail.com>
2021-12-31 17:10:44 -0500

    Ignore ctrl-z in fzf to avoid hanging
    Refer to https://github.com/junegunn/fzf/issues/2289 for more
    information.

--- fzf-git.bash
+++ fzf-git.bash
@@ -8,7 +8,7 @@ __fzf_git_is_in_git_repo() {
 __fzf_git_files() {
   __fzf_git_is_in_git_repo || return
   git -c color.status=always status --short |
-  fzf -m --ansi --nth 2..,.. \
+  fzf --bind=ctrl-z:ignore -m --ansi --nth 2..,.. \
     --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1})' |
   cut -c4- | sed 's/.* -> //'
 }
@@ -16,7 +16,7 @@ __fzf_git_files() {
 __fzf_git_branches() {
   __fzf_git_is_in_git_repo || return
   git branch -a --color=always | grep -v '/HEAD\s' | sort |
-  fzf --ansi --multi --tac --preview-window right:70% \
+  fzf --bind=ctrl-z:ignore --ansi --multi --tac --preview-window right:70% \
     --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' |
   sed 's/^..//' | cut -d' ' -f1 |
   sed 's#^remotes/##'
@@ -25,14 +25,14 @@ __fzf_git_branches() {
 __fzf_git_tags() {
   __fzf_git_is_in_git_repo || return
   git tag --sort -version:refname |
-  fzf --multi --preview-window right:70% \
+  fzf --bind=ctrl-z:ignore --multi --preview-window right:70% \
     --preview 'git show --color=always {}'
 }
 
 __fzf_git_hashes() {
   __fzf_git_is_in_git_repo || return
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+  fzf --bind=ctrl-z:ignore --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
   grep -o "[a-f0-9]\{7,\}"
@@ -41,13 +41,13 @@ __fzf_git_hashes() {
 __fzf_git_remotes() {
   __fzf_git_is_in_git_repo || return
   git remote -v | awk '{print $1 "\t" $2}' | uniq |
-  fzf --tac \
+  fzf --bind=ctrl-z:ignore --tac \
     --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1}' |
   cut -d$'\t' -f1
 }
 
 __fzf_git_stashes() {
   __fzf_git_is_in_git_repo || return
-  git stash list | fzf --reverse -d: --preview 'git show --color=always {1}' |
+  git stash list | fzf --bind=ctrl-z:ignore --reverse -d: --preview 'git show --color=always {1}' |
   cut -d: -f1
 }
